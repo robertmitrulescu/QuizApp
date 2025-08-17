3c700bd2e1baa0455e330ac1150f62ba58ff4311
Mitrulescu <robertmitrulescu@icloud.com>
2025-08-17 19:38:39 +0300

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Robert Paul Mitrulescu on 17.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuizTests/QuizTests.swift
@@ -0,0 +1,16 @@
+//
+//  QuizTests.swift
+//  QuizTests
+//
+//  Created by Robert Paul Mitrulescu on 17.08.2025.
+//
+
+import Testing
+
+struct QuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
